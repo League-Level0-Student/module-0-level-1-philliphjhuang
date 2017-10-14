b7d22824b64a2b3900fb2ded0fde4b00db1122ae
philliphjhuang <league@WTS22.attlocal.net>
2017-10-13 18:28:35 -0700

    unbirthday (IP)
    .

---
+++ src/Unbirthday.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String{}args)
+
+	{
+	String name = JOptionPane.showInputDialog("When is your birthday? (mm/dd)");
+	if(name = 10/13){
+		show("Happy Birthday");
+		else{
+			show("Merry UNbirthday");
+		}
+}
+}
+}
